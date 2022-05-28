e57d522f42363f5ecf18fb1a677ba1af39f6c4db
jxbx <bailey.justin@gmail.com>
2022-05-28 16:41:16 +0100

    added showCard()

--- filmlister.js
+++ filmlister.js
@@ -2,6 +2,7 @@
 
 const searchList = document.getElementById("searchList");
 const searchBar = document.getElementById("searchBar");
+const cardContainer = document.getElementById("cardContainer");
 
 searchBar.addEventListener("input", clearSearch);
 searchBar.addEventListener("input", populate);
@@ -24,7 +25,6 @@ async function populate() {
     const request = new Request(requestURL);
     const response = await fetch(request);
     const movies = await response.json();
-    console.log(movies);
 
 
 
@@ -35,17 +35,60 @@ async function populate() {
       const originalLanguage = (movies.results[i].original_language === "en") ? "" : movies.results[i].original_title + ", ";
 
 
-      const searchItem = document.createElement("option");
-      searchItem.class = "searchItem";
-      searchItem.value = title;
-      searchItem.label = originalLanguage + date;
-      searchItem.dataset.filmId = movies.results[i].id;
+      const searchItem = document.createElement("li");
+
+      searchItem.setAttribute("class", "searchItem");
+
+      const searchItemTitle = document.createElement("p");
+      searchItemTitle.setAttribute("class", "searchItemTitle");
+      searchItemTitle.innerHTML = title
+
+      const searchItemMoreInfo = document.createElement("p");
+      searchItemMoreInfo.setAttribute ("class", "searchItemMoreInfo");
+      searchItemMoreInfo.innerHTML = originalLanguage + " " + date;
+
+      searchItem.appendChild(searchItemTitle);
+      searchItem.appendChild(searchItemMoreInfo);
+      searchItem.value = movies.results[i].id;
+
+
       searchList.appendChild(searchItem);
+      searchItem.onclick = showCard;
     }
   }
 }
 
-function showCard(value) {
-  console.log(value);
+async function showCard() {
+
+
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.lastChild);
+  }
+
+
+  const url = "https://api.themoviedb.org/3/movie/" + this.value + "?api_key=f99dcdb2604d84233a9cf4e3f614828a";
+  const request = new Request(url);
+  const response = await fetch(request);
+  const movie = await response.json();
+
+  const moreInfo = [];
+
+  moreInfo.push(movie.title);
+  moreInfo.push(movie.release_date.slice(0,4));
+  moreInfo.push(movie.original_language);
+  moreInfo.push(movie.overview);
+  moreInfo.push(movie.poster_path);
+
+  const cardInfo = document.createElement("ul");
+  cardInfo.setAttribute("class", "cardInfo");
+
+  for (let i=0; i<moreInfo.length-1; i++){
+    const cardItem = document.createElement("li");
+    cardItem.setAttribute("class", "cardItem");
+    cardItem.innerHTML = moreInfo[i];
+    cardInfo.appendChild(cardItem);
+  }
+
+  cardContainer.appendChild(cardInfo);
 
 }
