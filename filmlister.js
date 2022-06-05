a88c76d858ccee857ccd7a647574371f641e0c1d
jxbx <bailey.justin@gmail.com>
2022-06-05 21:53:55 +0100

    added item cards

--- filmlister.js
+++ filmlister.js
@@ -33,11 +33,18 @@ async function populate() {
       const title = movies.results[i].title;
       const date = movies.results[i].release_date.slice(0,4);
       const originalLanguage = (movies.results[i].original_language === "en") ? "" : movies.results[i].original_title + ", ";
+      const posterPath = movies.results[i].poster_path;
 
 
       const searchItem = document.createElement("li");
 
       searchItem.setAttribute("class", "searchItem");
+
+      const searchItemImg = document.createElement("img");
+      searchItemImg.src = "https://image.tmdb.org/t/p/w92/" + posterPath;
+      searchItemImg.setAttribute("class", "posterThumbnail");
+
+      const textContainer = document.createElement("div")
 
       const searchItemTitle = document.createElement("p");
       searchItemTitle.setAttribute("class", "searchItemTitle");
@@ -47,8 +54,10 @@ async function populate() {
       searchItemMoreInfo.setAttribute ("class", "searchItemMoreInfo");
       searchItemMoreInfo.innerHTML = originalLanguage + " " + date;
 
-      searchItem.appendChild(searchItemTitle);
-      searchItem.appendChild(searchItemMoreInfo);
+      searchItem.appendChild(searchItemImg);
+      textContainer.appendChild(searchItemTitle);
+      textContainer.appendChild(searchItemMoreInfo);
+      searchItem.appendChild(textContainer);
       searchItem.value = movies.results[i].id;
 
 
@@ -64,31 +73,79 @@ async function showCard() {
   while (cardContainer.firstChild) {
     cardContainer.removeChild(cardContainer.lastChild);
   }
-
+  console.log(this.value);
 
   const url = "https://api.themoviedb.org/3/movie/" + this.value + "?api_key=f99dcdb2604d84233a9cf4e3f614828a";
   const request = new Request(url);
   const response = await fetch(request);
   const movie = await response.json();
 
+  console.log(movie.original_title);
+
   const moreInfo = [];
 
+
+
   moreInfo.push(movie.title);
+  if(movie.original_language !== "en") moreInfo.push(movie.original_title);
   moreInfo.push(movie.release_date.slice(0,4));
-  moreInfo.push(movie.original_language);
+  if (movie.spoken_languages.length >= 1) moreInfo.push(movie.spoken_languages[0].english_name);
+  moreInfo.push
+
   moreInfo.push(movie.overview);
-  moreInfo.push(movie.poster_path);
+
+
+  const cardImg = document.createElement("img");
+  cardImg.src = "https://image.tmdb.org/t/p/w780" + movie.backdrop_path;
+  cardImg.setAttribute("id", "cardImg");
 
   const cardInfo = document.createElement("ul");
-  cardInfo.setAttribute("class", "cardInfo");
+  cardInfo.setAttribute("id", "cardInfo");
 
-  for (let i=0; i<moreInfo.length-1; i++){
+  for (let i=0; i<moreInfo.length; i++){
     const cardItem = document.createElement("li");
     cardItem.setAttribute("class", "cardItem");
-    cardItem.innerHTML = moreInfo[i];
+    cardItem.innerHTML = decodeURI(moreInfo[i]);
     cardInfo.appendChild(cardItem);
   }
 
-  cardContainer.appendChild(cardInfo);
+  const confirmButton = document.createElement("button");
+  const cancelButton = document.createElement("button");
 
+
+
+  confirmButton.innerHTML = "confirm";
+  cancelButton.innerHTML = "cancel";
+  confirmButton.addEventListener("click", showForm);
+  cancelButton.addEventListener("click", clearCard);
+
+  cardContainer.appendChild(cardImg);
+  cardContainer.appendChild(cardInfo);
+  cardContainer.appendChild(confirmButton);
+  cardContainer.appendChild(cancelButton);
+}
+
+
+function showForm() {
+  cardImg.remove();
+  while (cardInfo.childNodes.length > 1) {
+    cardInfo.removeChild(cardInfo.lastChild);
+  }
+  while(cardContainer.childNodes.length > 1){
+    cardContainer.removeChild(cardContainer.lastChild);
+  }
+  const dateInput = document.createElement("input");
+  dateInput.setAttribute("type", "date");
+  dateInput.setAttribute("id", "dateInput");
+  cardInfo.appendChild(dateInput);
+
+  const reviewInput = document.createElement("textarea");
+  reviewInput.setAttribute("id", "reviewInput");
+  cardInfo.appendChild(reviewInput);
+}
+
+function clearCard() {
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.lastChild);
+  }
 }
