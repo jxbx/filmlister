b1e3a5cefc0f8b763a0ea7954fa19cacdf5e35de
jxbx <bailey.justin@gmail.com>
2022-07-18 13:46:23 +0100

    updated with sort function

--- filmlister.js
+++ filmlister.js
@@ -1,14 +1,32 @@
-
-
 const searchList = document.getElementById("searchList");
 const searchBar = document.getElementById("searchBar");
+const searchContainer = document.getElementById("searchContainer");
 const cardContainer = document.getElementById("cardContainer");
+const filmSubmitForm = document.getElementById("filmSubmitForm");
+const dateInput = document.getElementById("dateInput");
+const reviewInput = document.getElementById("reviewInput");
+const ratingInput = document.getElementById("ratingInput")
+const stars = document.getElementsByClassName("stars");
+const starLabels = document.getElementsByClassName("starLabel");
+const formTitle = document.getElementById("formTitle");
+const cancelForm = document.getElementById("cancelForm");
+const diaryItemContainer = document.getElementById("diaryItemContainer");
+const sortOptions = document.getElementById("sortOptions");
+const addToDiary = document.getElementById("addToDiary");
+
+filmSubmitForm.addEventListener("submit", submitForm);
+cancelForm.addEventListener("click", hideForm);
 
 searchBar.addEventListener("input", clearSearch);
 searchBar.addEventListener("input", populate);
 
+addToDiary.addEventListener("click", submitForm);
+
 let currentId = "";
-let diaryContent = [];
+let currentTitle = "";
+
+
+let diaryContent = justinDiary;
 
 function clearSearch() {
   while (searchList.hasChildNodes()) {
@@ -17,10 +35,9 @@ function clearSearch() {
 }
 
 async function populate() {
-  if (searchBar.value === ""){
+  if (searchBar.value === "") {
     return;
-  }
-  else {
+  } else {
     const firstPartURL = "https://api.themoviedb.org/3/search/movie?api_key=f99dcdb2604d84233a9cf4e3f614828a&language=en-US&query=";
     let query = searchBar.value;
 
@@ -29,15 +46,12 @@ async function populate() {
     const response = await fetch(request);
     const movies = await response.json();
 
-
-
-    for (let i=0; i<movies.results.length; i++){
+    for (let i = 0; i < 10; i++) {
 
       const title = movies.results[i].title;
-      const date = movies.results[i].release_date.slice(0,4);
+      const date = movies.results[i].release_date.slice(0, 4);
       const originalLanguage = (movies.results[i].original_language === "en") ? "" : movies.results[i].original_title + ", ";
       const posterPath = movies.results[i].poster_path;
-
 
       const searchItem = document.createElement("li");
 
@@ -54,7 +68,7 @@ async function populate() {
       searchItemTitle.innerHTML = title
 
       const searchItemMoreInfo = document.createElement("p");
-      searchItemMoreInfo.setAttribute ("class", "searchItemMoreInfo");
+      searchItemMoreInfo.setAttribute("class", "searchItemMoreInfo");
       searchItemMoreInfo.innerHTML = originalLanguage + " " + date;
 
       searchItem.appendChild(searchItemImg);
@@ -63,7 +77,6 @@ async function populate() {
       searchItem.appendChild(textContainer);
       searchItem.value = movies.results[i].id;
 
-
       searchList.appendChild(searchItem);
       searchItem.onclick = showCard;
     }
@@ -71,7 +84,7 @@ async function populate() {
 }
 
 async function showCard() {
-
+  searchContainer.style.display = "none";
   while (cardContainer.firstChild) {
     cardContainer.removeChild(cardContainer.lastChild);
   }
@@ -82,39 +95,37 @@ async function showCard() {
   const movie = await response.json();
 
   currentId = movie.id;
+  currentTitle = movie.title;
 
   let moreInfo = [];
 
   moreInfo.push(movie.title);
-  if(movie.original_language !== "en") moreInfo.push(movie.original_title);
-  moreInfo.push(movie.release_date.slice(0,4));
+  if (movie.original_language !== "en" && movie.original_title !== movie.title) moreInfo.push(movie.original_title);
+  moreInfo.push(movie.release_date.slice(0, 4));
   if (movie.spoken_languages.length >= 1) moreInfo.push(movie.spoken_languages[0].english_name);
-
   moreInfo.push(movie.overview);
 
   const cardImg = document.createElement("img");
   cardImg.src = "https://image.tmdb.org/t/p/w780" + movie.backdrop_path;
-  cardImg.setAttribute("id", "cardImg");
+  cardImg.setAttribute("class", "cardImg");
 
   const cardInfo = document.createElement("ul");
-  cardInfo.setAttribute("id", "cardInfo");
+  cardInfo.setAttribute("class", "cardInfo");
 
-  for (let i=0; i<moreInfo.length; i++){
+  for (let i = 0; i < moreInfo.length; i++) {
     const cardItem = document.createElement("li");
     cardItem.setAttribute("class", "cardItem");
-    cardItem.innerHTML = decodeURI(moreInfo[i]);
+    cardItem.innerHTML = moreInfo[i];
     cardInfo.appendChild(cardItem);
   }
 
   const confirmButton = document.createElement("button");
   const cancelButton = document.createElement("button");
 
-
-
-  confirmButton.innerHTML = "confirm";
-  cancelButton.innerHTML = "cancel";
+  confirmButton.innerText = "confirm";
+  cancelButton.innerText = "back";
   confirmButton.addEventListener("click", showForm);
-  cancelButton.addEventListener("click", clearCard);
+  cancelButton.addEventListener("click", clearAll);
 
   cardContainer.appendChild(cardImg);
   cardContainer.appendChild(cardInfo);
@@ -122,31 +133,282 @@ async function showCard() {
   cardContainer.appendChild(cancelButton);
 }
 
-
 function showForm() {
-  cardImg.remove();
-  while (cardInfo.childNodes.length > 1) {
-    cardInfo.removeChild(cardInfo.lastChild);
-  }
-  while(cardContainer.childNodes.length > 1){
-    cardContainer.removeChild(cardContainer.lastChild);
-  }
-  const dateInput = document.createElement("input");
-  dateInput.setAttribute("type", "date");
-  dateInput.setAttribute("id", "dateInput");
-  cardInfo.appendChild(dateInput);
-
-  const reviewInput = document.createElement("textarea");
-  reviewInput.setAttribute("id", "reviewInput");
-  cardInfo.appendChild(reviewInput);
+  cardContainer.style.display = "none";
+  formContainer.style.display = "flex";
+  formTitle.innerText = currentTitle;
 }
 
-function clearCard() {
+function hideForm() {
+
+  filmSubmitForm.reset();
+  cardContainer.style.display = "inline";
+  formContainer.style.display = "none";
+}
+
+function clearAll() {
   while (cardContainer.firstChild) {
     cardContainer.removeChild(cardContainer.lastChild);
   }
+  searchContainer.style.display = "block";
+  ratingInput.value = 0;
+  document.getElementById("ratingYellowBg").style.width = 0;
 }
 
-function addToDiary() {
- diaryContent.push(currentId);
+function submitForm() {
+
+  diaryContent.push({
+    "id": currentId,
+    "date": dateInput.value,
+    "review": reviewInput.value,
+    "rating": Number(ratingInput.value),
+    "title": currentTitle
+  });
+  clearAll();
+  hideForm();
+  updateDiary();
+}
+
+async function updateDiary() {
+
+  while (diaryList.firstChild) {
+    diaryList.removeChild(diaryList.lastChild);
+  }
+
+  sortDiary();
+
+  for (const entry of diaryContent){
+    const url = "https://api.themoviedb.org/3/movie/" + entry.id + "?api_key=f99dcdb2604d84233a9cf4e3f614828a";
+    const request = new Request(url);
+    const response = await fetch(request);
+    const movie = await response.json();
+
+    const date = movie.release_date.slice(0, 4);
+    const originalLanguage = (movie.original_language === "en" || movie.title === movie.original_title) ? "" : movie.original_title + ", ";
+    const viewedDate = new Date(entry.date).toString().slice(3,15);
+
+    const diaryItem = document.createElement("li");
+
+    diaryItem.setAttribute("class", "diaryItem");
+    diaryItem.setAttribute("id", entry.id);
+    diaryItem.setAttribute("onclick", "showDiaryItem(this)")
+
+    const dateAndOrder = document.createElement("div");
+    dateAndOrder.setAttribute("class", "dateAndOrder");
+
+    const viewOrder = document.createElement("p");
+    viewOrder.setAttribute("class", "viewOrder");
+
+    switch(sortOptions.options[sortOptions.selectedIndex].value){
+      case "dateNewest": viewOrder.innerHTML = diaryContent.length - diaryContent.indexOf(entry);
+      break;
+      case "dateOldest":  viewOrder.innerHTML = diaryContent.indexOf(entry) +1;
+      break;
+      case "ratingLowest":
+        if (diaryContent.indexOf(entry) === 0){
+          viewOrder.innerHTML = (Number.isInteger(entry.rating)) ? entry.rating + "★" : "";
+        }
+        else if (Math.floor(entry.rating) > Math.floor(diaryContent[diaryContent.indexOf(entry)-1].rating)){
+          viewOrder.innerHTML = entry.rating + "★";
+        }
+        else {
+          viewOrder.innerHTML = "";
+        }
+        break;
+      case "ratingHighest":
+        if (diaryContent.indexOf(entry) === 0){
+          viewOrder.innerHTML = (Number.isInteger(entry.rating)) ? entry.rating + "★" : "";
+        }
+        else if (Math.ceil(diaryContent[diaryContent.indexOf(entry)-1].rating) > Math.ceil(entry.rating)){
+          viewOrder.innerHTML = entry.rating + "★";
+        }
+        else {
+          viewOrder.innerHTML = "";
+        }
+      break;
+      case "az":
+      case "za":
+        if (diaryContent.indexOf(entry) === 0 || diaryContent[diaryContent.indexOf(entry)-1].title.slice(0,1) !== entry.title.slice(0,1)){
+          viewOrder.innerHTML = entry.title.slice(0,1)
+        }
+        else {
+          viewOrder.innerHTML = "";
+        }
+      }
+
+    dateAndOrder.appendChild(viewOrder);
+
+    const diaryItemImg = document.createElement("img");
+    diaryItemImg.src = "https://image.tmdb.org/t/p/w92/" + movie.poster_path;
+    diaryItemImg.setAttribute("class", "posterThumbnail");
+
+    const textContainer = document.createElement("div")
+
+    const diaryItemTitle = document.createElement("p");
+    diaryItemTitle.setAttribute("class", "diaryItemTitle");
+    diaryItemTitle.innerHTML = movie.title
+
+    const diaryItemMoreInfo = document.createElement("p");
+    diaryItemMoreInfo.setAttribute("class", "diaryItemMoreInfo");
+    diaryItemMoreInfo.innerHTML = originalLanguage + " " + date + "<br>" + printRating(entry) + "<br>" + "viewed on " + viewedDate;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "×";
+    removeButton.setAttribute("class", "removeButton")
+    removeButton.addEventListener("click", function () {
+      diaryContent = diaryContent.filter(item => item.id != entry.id);
+      document.getElementById(entry.id).remove();
+      event.stopPropagation();
+      updateDiary();
+    });
+    textContainer.appendChild(diaryItemTitle);
+    textContainer.appendChild(diaryItemMoreInfo);
+
+    diaryItem.appendChild(dateAndOrder);
+    diaryItem.appendChild(diaryItemImg);
+    diaryItem.appendChild(textContainer);
+    diaryItem.appendChild(removeButton);
+
+    diaryList.appendChild(diaryItem);
+
+    currentId = "";
+    currentTitle = "";
+  }
+}
+
+function printRating(input) {
+  let rating = "";
+  let floorRating = Math.floor(input.rating);
+
+  for (let i=0; i<floorRating; i++){
+    rating +="★";
+  }
+  if (!Number.isInteger(input.rating)){
+    rating += "½";
+  }
+  return rating
+}
+
+async function showDiaryItem(input) {
+  diaryList.style.display = "none";
+  sortContainer.style.display = "none"
+
+  const entry = diaryContent.find(element => element.id == input.id);
+
+  const url = "https://api.themoviedb.org/3/movie/" + input.id + "?api_key=f99dcdb2604d84233a9cf4e3f614828a";
+  const request = new Request(url);
+  const response = await fetch(request);
+  const movie = await response.json();
+
+  let moreInfo = [];
+
+  moreInfo.push(movie.title);
+  if (movie.original_language !== "en" && movie.title !== movie.original_title) moreInfo.push(movie.original_title);
+  moreInfo.push(movie.release_date.slice(0, 4));
+  if (movie.spoken_languages.length >= 1) moreInfo.push(movie.spoken_languages[0].english_name);
+  moreInfo.push(printRating(entry));
+  (entry.review.length === 0) ? moreInfo.push("no review yet") : moreInfo.push(entry.review);
+
+  const diaryImg = document.createElement("img");
+  diaryImg.src = "https://image.tmdb.org/t/p/w780" + movie.backdrop_path;
+  diaryImg.setAttribute("class", "cardImg");
+
+  const diaryInfo = document.createElement("ul");
+  diaryInfo.setAttribute("class", "cardInfo");
+
+  for (let i = 0; i < moreInfo.length; i++) {
+    const diaryItem = document.createElement("li");
+    diaryItem.setAttribute("class", "cardItem");
+    diaryItem.innerHTML = moreInfo[i];
+    diaryInfo.appendChild(diaryItem);
+  }
+
+  const backButton = document.createElement("button");
+
+  backButton.innerText = "back";
+  backButton.addEventListener("click", clearDiaryItemContainer);
+
+  diaryItemContainer.appendChild(diaryImg);
+  diaryItemContainer.appendChild(diaryInfo);
+  diaryItemContainer.appendChild(backButton);
+}
+
+function clearDiaryItemContainer() {
+  while (diaryItemContainer.firstChild) {
+    diaryItemContainer.removeChild(diaryItemContainer.lastChild);
+  }
+  diaryList.style.display = "block";
+  sortContainer.style.display = "block";
+}
+
+window.onload = function () {
+    updateDiary();
+}
+
+function rate(input) {
+  document.getElementById("ratingYellowBg").style.width = input.value*1.71 + "rem";
+  validateForm();
+}
+
+function validateForm(){
+    addToDiary.disabled = (ratingInput.value <= 0 || !dateInput.value) ? true : false;
+}
+
+function sortDiary() {
+
+let selection = sortOptions.options[sortOptions.selectedIndex].value;
+
+  switch(selection){
+    case "dateNewest":
+      diaryContent.sort((a,b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+    break;
+    case "dateOldest":
+      diaryContent.sort((a,b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
+    break;
+    case "ratingHighest":
+      diaryContent.sort((a,b) => {
+        return b.rating - a.rating;
+      });
+    break;
+    case "ratingLowest":
+      diaryContent.sort((a,b) => {
+        return a.rating - b.rating;
+      });
+    break;
+    case "az":
+      diaryContent.sort((a,b) => {
+        if (a.title < b.title) {return -1;}
+        if (a.title > b.title) {return 1;}
+        return 0;
+      });
+    break;
+    case "za":
+      diaryContent.sort((a,b) => {
+        if (a.title < b.title) {return 1;}
+        if (a.title > b.title) {return -1;}
+        return 0;
+      });
+  }
+}
+
+
+
+async function languages(input) {
+
+  let array = [];
+
+  for (const item of diaryContent){
+    const url = "https://api.themoviedb.org/3/movie/" + item.id + "?api_key=f99dcdb2604d84233a9cf4e3f614828a";
+    const request = new Request(url);
+    const response = await fetch(request);
+    const movie = await response.json();
+
+    array.push(movie.original_language);
+  }
+  return array;
+
 }
---
+++ justindiary.js
@@ -0,0 +1,429 @@
+const justinDiary = [
+    {
+        "id": 4024,
+        "date": "2022-07-06",
+        "review": "",
+        "rating": 2.5,
+        "title": "Last Year at Marienbad"
+    },
+    {
+        "id": 13537,
+        "date": "2022-06-30",
+        "review": "",
+        "rating": 3.5,
+        "title": "Shattered Glass"
+    },
+    {
+        "id": 9303,
+        "date": "2022-06-29",
+        "review": "",
+        "rating": 4,
+        "title": "Bound"
+    },
+    {
+        "id": 252171,
+        "date": "2022-06-28",
+        "review": "",
+        "rating": 3,
+        "title": "A Girl Walks Home Alone at Night"
+    },
+    {
+        "id": 63892,
+        "date": "2022-06-22",
+        "review": "",
+        "rating": 3,
+        "title": "Léon Morin, Priest"
+    },
+    {
+        "id": 11401,
+        "date": "2022-06-21",
+        "review": "",
+        "rating": 3.5,
+        "title": "Metallica: Some Kind Of Monster"
+    },
+    {
+        "id": 1949,
+        "date": "2022-06-15",
+        "review": "",
+        "rating": 3.5,
+        "title": "Zodiac"
+    },
+    {
+        "id": 43344,
+        "date": "2022-06-09",
+        "review": "",
+        "rating": 3.5,
+        "title": "Él"
+    },
+    {
+        "id": 780609,
+        "date": "2022-06-08",
+        "review": "",
+        "rating": 4,
+        "title": "Men"
+    },
+    {
+        "id": 8214,
+        "date": "2022-06-06",
+        "review": "",
+        "rating": 3,
+        "title": "Drifting Clouds"
+    },
+    {
+        "id": 20108,
+        "date": "2022-06-05",
+        "review": "",
+        "rating": 3,
+        "title": "Au Hasard Balthazar"
+    },
+    {
+        "id": 2,
+        "date": "2022-06-05",
+        "review": "",
+        "rating": 3.5,
+        "title": "Ariel"
+    },
+    {
+        "id": 11225,
+        "date": "2022-06-04",
+        "review": "",
+        "rating": 4,
+        "title": "Into Great Silence"
+    },
+    {
+        "id": 29449,
+        "date": "2022-06-03",
+        "review": "",
+        "rating": 1,
+        "title": "Images"
+    },
+    {
+        "id": 294741,
+        "date": "2022-06-02",
+        "review": "",
+        "rating": 3.5,
+        "title": "The Falling"
+    },
+    {
+        "id": 487558,
+        "date": "2022-05-31",
+        "review": "",
+        "rating": 3,
+        "title": "BlacKkKlansman"
+    },
+    {
+        "id": 195841,
+        "date": "2022-05-30",
+        "review": "",
+        "rating": 3,
+        "title": "Oh, Sun"
+    },
+    {
+        "id": 40751,
+        "date": "2022-05-29",
+        "review": "",
+        "rating": 2.5,
+        "title": "Ashes of Time"
+    },
+    {
+        "id": 423646,
+        "date": "2022-05-22",
+        "review": "",
+        "rating": 2,
+        "title": "In the Fade"
+    },
+    {
+        "id": 8488,
+        "date": "2022-05-20",
+        "review": "",
+        "rating": 2,
+        "title": "Hitch"
+    },
+    {
+        "id": 2748,
+        "date": "2022-05-19",
+        "review": "",
+        "rating": 3.5,
+        "title": "Journey to Italy"
+    },
+    {
+        "id": 347823,
+        "date": "2022-05-18",
+        "review": "",
+        "rating": 3,
+        "title": "Another Country"
+    },
+    {
+        "id": 42506,
+        "date": "2022-05-10",
+        "review": "",
+        "rating": 2.5,
+        "title": "The Housemaid"
+    },
+    {
+        "id": 709,
+        "date": "2022-05-08",
+        "review": "",
+        "rating": 2,
+        "title": "Licence to Kill"
+    },
+    {
+        "id": 454527,
+        "date": "2022-05-06",
+        "review": "",
+        "rating": 3,
+        "title": "Benedetta"
+    },
+    {
+        "id": 660120,
+        "date": "2022-05-02",
+        "review": "",
+        "rating": 2.5,
+        "title": "The Worst Person in the World"
+    },
+    {
+        "id": 674324,
+        "date": "2022-04-27",
+        "review": "",
+        "rating": 3.5,
+        "title": "The Banshees of Inisherin"
+    },
+    {
+        "id": 4982,
+        "date": "2022-04-27",
+        "review": "",
+        "rating": 3,
+        "title": "American Gangster"
+    },
+    {
+        "id": 648579,
+        "date": "2022-04-24",
+        "review": "",
+        "rating": 2,
+        "title": "The Unbearable Weight of Massive Talent"
+    },
+    {
+        "id": 728526,
+        "date": "2022-04-24",
+        "review": "",
+        "rating": 3,
+        "title": "Encounter"
+    },
+    {
+        "id": 121986,
+        "date": "2022-04-22",
+        "review": "",
+        "rating": 3.5,
+        "title": "Frances Ha"
+    },
+    {
+        "id": 146233,
+        "date": "2022-04-18",
+        "review": "",
+        "rating": 4.5,
+        "title": "Prisoners"
+    },
+    {
+        "id": 181886,
+        "date": "2022-04-18",
+        "review": "",
+        "rating": 2,
+        "title": "Enemy"
+    },
+    {
+        "id": 179144,
+        "date": "2022-04-17",
+        "review": "",
+        "rating": 2.5,
+        "title": "The Great Beauty"
+    },
+    {
+        "id": 712167,
+        "date": "2022-04-15",
+        "review": "",
+        "rating": 4,
+        "title": "Beginning"
+    },
+    {
+        "id": 837745,
+        "date": "2022-04-11",
+        "review": "",
+        "rating": 4.5,
+        "title": "Small Body"
+    },
+    {
+        "id": 502033,
+        "date": "2022-04-10",
+        "review": "",
+        "rating": 4,
+        "title": "Sound of Metal"
+    },
+    {
+        "id": 568790,
+        "date": "2022-04-08",
+        "review": "",
+        "rating": 3.5,
+        "title": "Ghost Town Anthology"
+    },
+    {
+        "id": 634528,
+        "date": "2022-04-04",
+        "review": "",
+        "rating": 3,
+        "title": "The Marksman"
+    },
+    {
+        "id": 548544,
+        "date": "2022-04-01",
+        "review": "",
+        "rating": 3,
+        "title": "Invisible Life"
+    },
+    {
+        "id": 630240,
+        "date": "2022-03-31",
+        "review": "",
+        "rating": 2.5,
+        "title": "Titane"
+    },
+    {
+        "id": 378018,
+        "date": "2022-03-19",
+        "review": "",
+        "rating": 2.5,
+        "title": "The Void"
+    },
+    {
+        "id": 520763,
+        "date": "2022-03-19",
+        "review": "",
+        "rating": 2.5,
+        "title": "A Quiet Place Part II"
+    },
+    {
+        "id": 577922,
+        "date": "2022-03-09",
+        "review": "",
+        "rating": 4,
+        "title": "Tenet"
+    },
+    {
+        "id": 49014,
+        "date": "2022-03-09",
+        "review": "",
+        "rating": 3,
+        "title": "Cosmopolis"
+    },
+    {
+        "id": 414906,
+        "date": "2022-03-08",
+        "review": "",
+        "rating": 3.5,
+        "title": "The Batman"
+    },
+    {
+        "id": 661365,
+        "date": "2022-03-07",
+        "review": "",
+        "rating": 3.5,
+        "title": "Ali & Ava"
+    },
+    {
+        "id": 788929,
+        "date": "2022-03-06",
+        "review": "",
+        "rating": 1.5,
+        "title": "Lamb"
+    },
+    {
+        "id": 336222,
+        "date": "2022-03-05",
+        "review": "",
+        "rating": 4,
+        "title": "Rams"
+    },
+    {
+        "id": 22613,
+        "date": "2022-03-01",
+        "review": "",
+        "rating": 2,
+        "title": "The Outlaw"
+    },
+    {
+        "id": 10633,
+        "date": "2022-02-26",
+        "review": "",
+        "rating": 3,
+        "title": "In the Heat of the Night"
+    },
+    {
+        "id": 794782,
+        "date": "2022-02-25",
+        "review": "",
+        "rating": 3.5,
+        "title": "La Mif"
+    },
+    {
+        "id": 152532,
+        "date": "2022-02-19",
+        "review": "",
+        "rating": 3,
+        "title": "Dallas Buyers Club"
+    },
+    {
+        "id": 64131,
+        "date": "2022-02-11",
+        "review": "",
+        "rating": 2,
+        "title": "Mauvais Sang"
+    },
+    {
+        "id": 600583,
+        "date": "2022-02-10",
+        "review": "",
+        "rating": 2,
+        "title": "The Power of the Dog"
+    },
+    {
+        "id": 194662,
+        "date": "2022-02-07",
+        "review": "",
+        "rating": 3.5,
+        "title": "Birdman or (The Unexpected Virtue of Ignorance)"
+    },
+    {
+        "id": 575776,
+        "date": "2022-02-05",
+        "review": "",
+        "rating": 2.5,
+        "title": "Saint Maud"
+    },
+    {
+        "id": 1990,
+        "date": "2022-02-01",
+        "review": "",
+        "rating": 3.5,
+        "title": "Paranoid Park"
+    },
+    {
+        "id": 468,
+        "date": "2022-01-12",
+        "review": "",
+        "rating": 5,
+        "title": "My Own Private Idaho"
+    },
+    {
+        "id": 718032,
+        "date": "2022-01-09",
+        "review": "",
+        "rating": 2.5,
+        "title": "Licorice Pizza"
+    },
+    {
+        "id": 554230,
+        "date": "2022-01-02",
+        "review": "",
+        "rating": 4.5,
+        "title": "The Lost Daughter"
+    }
+]
